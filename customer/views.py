b18efab8b80e0e23e4e84acadd56532622c9df3b
joshuapanjaitan <joshuapanjaitan01@gmail.com>
2021-03-10 11:32:59 +0700

    auto refresh display

--- customer/views.py
+++ customer/views.py
@@ -6,10 +6,43 @@ from layanan.forms import CurrentForm
 
 
 def display(request):
-    queue = Current.objects.filter(status='active')
-    cek = Current.objects.filter(status='active')
+    context = {}
+    context['page_title'] = 'Display'
+    # bpjs
+    bpjs = Current.objects.filter(status='active').filter(jenis_layanan='A')
+    ct_bpjs = Current.objects.filter(
+        status='active').filter(jenis_layanan='A').count()
+    if ct_bpjs == 0:
+        context['bpjs'] = '#'
+    else:
+        context['bpjs'] = bpjs
+
+    # gigi
+    gigi = Current.objects.filter(status='active').filter(jenis_layanan='B')
+    ct_gigi = Current.objects.filter(
+        status='active').filter(jenis_layanan='B').count()
+
+    if ct_gigi == 0:
+        context['gigi'] = '#'
+    else:
+        context['gigi'] = gigi
+
+    # jantung
+    jantung = Current.objects.filter(status='active').filter(jenis_layanan='C')
+    ct_jantung = Current.objects.filter(
+        status='active').filter(jenis_layanan='C').count()
+
+    if ct_jantung == 0:
+        context['jantung'] = '#'
+    else:
+        context['jantung'] = jantung
+
+    """
     context = {
         'page_title': 'Display',
-        'queue': queue,
+        'bpjs': bpjs,
+        'gigi': gigi,
+        'jantung': jantung,
     }
+    """
     return render(request, 'customer/index.html', context)
